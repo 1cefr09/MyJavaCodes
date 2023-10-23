bcf941f97268995a62ab44534a36efc8654716f5
1cefr09 <wangxrmail@163.com>
2023-10-23 19:44:46 +0800

    1

---
+++ Chapter01/src/HelloWorld.java
@@ -0,0 +1,18 @@
+class HelloChina{
+    public static void main(String[] args){
+        System.out.println("hello,world!!");
+        System.out.print("hello,world!!");
+        System.out.println("123abc");
+        System.out.println(123 + 1);
+    }
+}
+//单行注释
+
+/*
+多行注释
+ */
+
+/**
+ * 文档注释内容可以被JDK提供的工具javadoc解析，生成一套以网页文件形式体现的该程序的说明文档
+ *
+ */
